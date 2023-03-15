cd53a5af130d57816d89f56136bfd2f1198e6a2a
zeynepsoyan <zeynepsoyan@gmail.com>
2023-03-15 16:48:36 +0300

    Initial commit

---
+++ src/main/java/dev/zhs/moviereviewapp/Movie.java
@@ -0,0 +1,32 @@
+package dev.zhs.moviereviewapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrop;
+
+/*  DB will store only the ids of the review and the reviews will be stored in a separate connection, preventing repetition
+    https://www.mongodb.com/docs/manual/tutorial/model-referenced-one-to-many-relationships-between-documents/ */
+    @DocumentReference
+    private List<Review> reviewIds;
+}
---
+++ src/test/java/dev/zhs/moviereviewapp/MovieReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zhs.moviereviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
