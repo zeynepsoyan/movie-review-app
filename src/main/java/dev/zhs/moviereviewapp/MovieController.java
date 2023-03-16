b52dfff8e7be1464d9c97abbe28f081979e6f48d
zeynepsoyan <zeynepsoyan@gmail.com>
2023-03-16 13:02:26 +0300

    Added post review

--- src/main/java/dev/zhs/moviereviewapp/MovieController.java
+++ src/main/java/dev/zhs/moviereviewapp/MovieController.java
@@ -1,13 +1,16 @@
 package dev.zhs.moviereviewapp;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -17,6 +20,11 @@ public class MovieController {
 
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
-        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId) {
+        return new ResponseEntity<>(movieService.movieById(imdbId), HttpStatus.OK);
     }
 }
--- src/main/java/dev/zhs/moviereviewapp/MovieService.java
+++ src/main/java/dev/zhs/moviereviewapp/MovieService.java
@@ -1,9 +1,11 @@
 package dev.zhs.moviereviewapp;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,6 +14,10 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> movieById(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 
 }
